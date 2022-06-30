078dce36920b00669c2627265d3f53b0795bfc59
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2022-06-30 16:57:24 +0430

    Add certificate authority

---
+++ certificate_authority.py
@@ -0,0 +1,17 @@
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+
+from rsa_signature import RSASignature
+from utils import convert_to_bytes
+
+
+class CertificateAuthority:
+    def __init__(self):
+        self.rsa_signature_scheme = RSASignature()
+
+    @property
+    def public_key(self):
+        return self.rsa_signature_scheme.public_key
+
+    def issue_certificate(self, certificate_owner_public_key: RSAPublicKey) -> bytes:
+        return self.rsa_signature_scheme.sign(convert_to_bytes(certificate_owner_public_key))
+
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 from cryptography.hazmat.primitives.asymmetric.dh import DHPublicKey
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 
 
-def convert_to_bytes(public_key: DHPublicKey) -> bytes:
+def convert_to_bytes(public_key: Union[DHPublicKey, RSAPublicKey]) -> bytes:
     return public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo    
