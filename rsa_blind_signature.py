baf83c89eb9ae29afc972997567cbcb47ab02322
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2022-07-01 11:28:10 +0430

    Modify rsa_signature to save and load keys from PEM files

--- rsa_blind_signature.py
+++ rsa_blind_signature.py
@@ -1,15 +1,10 @@
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPrivateKey
+
 import cryptomath
 import random
-from user import User
-
-from typing import Tuple
 
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
-
-PublicKey = Tuple[int, int]
-PrivateKey = Tuple[int, int]
 
 
 def find_relative_prime_number(n):
@@ -27,50 +22,26 @@ def digest_message_to_int(message: bytes):
     return int.from_bytes(hashed_message, byteorder='big')
 
 
-def generate_rsa() -> (PrivateKey, PublicKey):
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=512,
-        backend=default_backend()
-    )
-    public_key = private_key.public_key()
-    n = public_key.public_numbers().n
-    a, b = private_key.private_numbers().d, public_key.public_numbers().e
-    return (n, a), (n, b)
-
-
-def blind(message: int, signer_public_key: PublicKey) -> (int, int):
-    n, b = signer_public_key
+def blind(message: int, signer_public_key: RSAPublicKey) -> (int, int):
+    n, b = signer_public_key.public_numbers().n, signer_public_key.public_numbers().e
     opening_value = find_relative_prime_number(n)
     blinded_message = (message * pow(opening_value, b, n)) % n
     return opening_value, blinded_message
 
 
-def sign(blinded_message: int, signer_private_key: PrivateKey):
-    n, a = signer_private_key
+def sign(blinded_message: int, signer_private_key: RSAPrivateKey):
+    p, q = signer_private_key.private_numbers().p, signer_private_key.private_numbers().q
+    n, a = p * q, signer_private_key.private_numbers().d
     return pow(blinded_message, a, n)
 
 
-def unblind_signature(blind_signature: int, opening_value: int, signer_public_key: PublicKey):
-    n, b = signer_public_key
+def unblind_signature(blind_signature: int, opening_value: int, signer_public_key: RSAPublicKey):
+    n, b = signer_public_key.public_numbers().n, signer_public_key.public_numbers().e
     r_inv = pow(opening_value, -1, n)
     return (blind_signature * r_inv) % n
 
 
-def verify(message: int, message_signature: int, singer_public_key: PublicKey):
-    n, b = singer_public_key
+def verify(message: int, message_signature: int, singer_public_key: RSAPublicKey):
+    n, b = singer_public_key.public_numbers().n, singer_public_key.public_numbers().e
     return pow(message_signature, b, n) == message
 
-
-if __name__ == '__main__':
-    bob = User("Bob")
-    msg = bob.public_key.public_numbers().e
-    msg += bob.public_key.public_numbers().n
-
-    msg_hash = digest_message_to_int(msg.to_bytes(128, byteorder='big'))
-
-    sk, pk = generate_rsa()
-    r, blind_message = blind(msg_hash, pk)
-    signature = sign(blind_message, sk)
-    unblind_sign = unblind_signature(signature, r, pk)
-    print(verify(msg_hash, unblind_sign, pk))
--- rsa_signature.py
+++ rsa_signature.py
@@ -1,18 +1,14 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPrivateKey
 
 
 class RSASignature:
-    def __init__(self):
-        self.__private_key: RSAPrivateKey = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=512,
-            backend=default_backend()
-        )
-        self.__public_key: RSAPublicKey = self.__private_key.public_key()
+    def __init__(self, private_key: RSAPrivateKey, public_key: RSAPublicKey):
+        self.__private_key = private_key
+        self.__public_key = public_key
 
     @property
     def public_key(self):
@@ -29,6 +25,54 @@ class RSASignature:
             hashes.SHA256()
         )
         return signature
+
+    @staticmethod
+    def load_private_key(private_key_path: str):
+        with open(private_key_path, 'rb') as private_key_file:
+            private_key = serialization.load_pem_private_key(
+                private_key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
+        return private_key
+
+    @staticmethod
+    def load_public_key(public_key_path: str):
+        with open(public_key_path, 'rb') as public_key_file:
+            public_key = serialization.load_pem_public_key(
+                public_key_file.read(),
+                backend=default_backend()
+            )
+        return public_key
+
+    def save_private_key(self, private_key_path: str):
+        with open(private_key_path, 'wb') as private_key_file:
+            private_key_file.write(
+                self.__private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.TraditionalOpenSSL,
+                    encryption_algorithm=serialization.NoEncryption()
+                )
+            )
+
+    def save_public_key(self, public_key_path: str):
+        with open(public_key_path, 'wb') as public_key_file:
+            public_key_file.write(
+                self.__public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo
+                )
+            )
+
+    @staticmethod
+    def generate_rsa() -> (RSAPrivateKey, RSAPublicKey):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=512,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
+        return private_key, public_key
 
     @staticmethod
     def verify(signer_public_key: RSAPublicKey, message: bytes, signature: bytes):
